ba6ab45b0400607865b78fc85100bf25daad906d
Abelard Bautista <abelardbautista@gmail.com>
2020-02-01 13:46:09 -0800

    Disabled refresh on update function. Adjusted margin on header.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function update() {
   $("#month").text(moment().format("MMM"));
   $("#date").text(moment().format("DD"));
   $("#time").text(moment().format("dddd, h:mm:ss a"));
-  setInterval(update, 1000);
+  //   setInterval(update, 1000);
 }
 
 update();

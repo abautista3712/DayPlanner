dd653ce2bed574c95a7a340d444334f0bea863a3
Abelard Bautista <abelardbautista@gmail.com>
2020-02-02 23:25:56 -0800

    Added main content sections.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,8 +8,20 @@ update();
 
 var contentTime = $("<div>");
 contentTime.text("9AM");
-contentTime.css("flex", "1 0 0");
+contentTime.css("flex-grow", "1");
+contentTime.css("border-right", "solid");
+
+var contentInput = $("<div>");
+contentInput.text("BLANK");
+contentInput.css("flex-grow", "8");
+contentInput.css("border-right", "solid");
+
+var contentSave = $("<div>");
+contentSave.text("SAVE");
+contentSave.css("flex-grow", "1");
 
 $("#contentWrapper").append(contentTime);
+$("#contentWrapper").append(contentInput);
+$("#contentWrapper").append(contentSave);
 
 for (i = 0; i < 9; i++) {}

0497f0cc2cd1de82be9d698a08d3573ebc0548be
Abelard Bautista <abelardbautista@gmail.com>
2020-02-02 23:15:06 -0800

    Added content box.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,12 @@ function update() {
   $("#time").text(moment().format("dddd, h:mm:ss a"));
   //   setInterval(update, 1000);
 }
-
 update();
+
+var contentTime = $("<div>");
+contentTime.text("9AM");
+contentTime.css("flex", "1 0 0");
+
+$("#contentWrapper").append(contentTime);
+
+for (i = 0; i < 9; i++) {}
